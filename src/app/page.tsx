de7ee88ef8775aa089c54df1361a315cc8a910f9
Debashis Roy <goodquote39@hotmail.com>
2024-04-21 17:29:43 +0600

    new project started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,10 @@
-"use client";
-import themes from "@/lib/themes";
-import { useEffect, useState } from "react";
-import { ThemeProvider } from "./layout";
+import Image from "next/image";
 
 export default function Home() {
-  const [theme, setTheme] = useState(themes.theme1);
-  const handleTheme = (newTheme: any) => {
-    setTheme(newTheme);
-    localStorage.setItem("theme", newTheme);
-  };
-
-  // console.log(theme);
   return (
-    <ThemeProvider value={theme}>
-      <div>
-        <button onClick={() => handleTheme(themes.theme1.value)}>
-          Theme 1
-        </button>
-        <button onClick={() => handleTheme(themes.theme2.value)}>
-          Theme 2
-        </button>
-        <button onClick={() => handleTheme(themes.theme3.value)}>
-          Theme 3
-        </button>
-        <h1 className="text-3xl font-semibold">Hello World</h1>
-        <p>Test Theme project </p>
-        <div className="w-32 h-32 bg-primary"></div>
-      </div>
-    </ThemeProvider>
+    <div>
+      <h1>multiple color theme</h1>
+      <p>testing the idea</p>
+    </div>
   );
 }
