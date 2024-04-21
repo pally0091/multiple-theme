49a5eff00b324acf7e0147e76e3b6975de97582d
Debashis Roy <goodquote39@hotmail.com>
2024-04-21 16:22:21 +0600

    working on mutiple color theme

---
+++ src/lib/themes.ts
@@ -0,0 +1,16 @@
+const themes = {
+  theme1: {
+    name: "theme1",
+    value: "theme1",
+  },
+  theme2: {
+    name: "theme2",
+    value: "theme2",
+  },
+  theme3: {
+    name: "theme3",
+    value: "theme3",
+  },
+};
+
+export default themes;
