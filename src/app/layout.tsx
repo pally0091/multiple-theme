fff584446c1dc79989c2b3afb89bf97c49758670
Debashis Roy <goodquote39@hotmail.com>
2024-04-22 12:05:13 +0600

    Update project configuration and styles for multiple color themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Provider from "@/components/Provider/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <main>{children}</main>
+      <body className="">
+        <Provider>
+          <main>{children}</main>
+        </Provider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
-      <h1>multiple color theme</h1>
+      <h1 className="text-3xl font-semibold">multiple color theme</h1>
       <p>testing the idea</p>
     </div>
   );
