fa77fceaaa6b5c059e25d0d24ab0d5d5dee538b7
Debashis Roy <goodquote39@hotmail.com>
2024-04-22 13:11:24 +0600

    Update homepage with divs of primary and destructive background colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="">
+      <body className={inter.className}>
         <main>
           <ThemeProvider>{children}</ThemeProvider>
         </main>
