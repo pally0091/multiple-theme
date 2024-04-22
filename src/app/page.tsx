b6f47e294f7dd4606ee18ca5a1acd190d563635d
Debashis Roy <goodquote39@hotmail.com>
2024-04-22 13:18:15 +0600

    Added mx-auto class to center div elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-4xl font-semibold">Multicolor Test</h1>
-      <div className="w-32 h-32 bg-primary"></div>
-      <div className="w-32 h-32 bg-destructive"></div>
+      <div className="mx-auto w-32 h-32 bg-primary"></div>
+      <div className="mx-auto w-32 h-32 bg-destructive"></div>
     </div>
   );
 }
